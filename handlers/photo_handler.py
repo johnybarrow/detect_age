04693e40e30a670f7321e3111f361a27a4f7eeeb
ivan <johny.barrow@bk.ru>
2020-07-17 15:50:06 +0300

    add fast send photo by id and sleep before sending photo

--- handlers/photo_handler.py
+++ handlers/photo_handler.py
@@ -5,25 +5,27 @@ from .detect import get_age, get_new_photo, name_of_age
 from json import loads
 from random import randint
 import re
+import asyncio
+from os import listdir
 
 players_array = dict()
+all_files = dict()
 
 age_dataset = open('dataset/age_set.txt').readlines()
 
 
 @dp.message_handler(content_types=['photo'])
-async def handle_photo(message: types.Message):
-    file_id = message.photo[-1]['file_id']
+async def handle_photo(msg: types.Message):
+    file_id = msg.photo[-1]['file_id']
     response = loads(get(f'https://api.telegram.org/bot{TOKEN}/getFile?file_id={file_id}').text)
 
     if response['ok']:
         photo = get(f'https://api.telegram.org/file/bot{TOKEN}/{response["result"]["file_path"]}').content
         print(f'https://api.telegram.org/file/bot{TOKEN}/{response["result"]["file_path"]}')
-        await bot.send_photo(message.from_user.id, get_new_photo(photo), reply_to_message_id=message.message_id,
-                             caption=get_age(photo))
+        await bot.send_photo(msg.from_user.id, get_new_photo(photo), caption=get_age(photo))
     else:
-        await bot.send_photo(message.from_user.id, open('dataset/witcher.png'), reply_to_message_id=message.message_id,
-                             caption="Хмм, зараза... Что-то не так, возможно фотка слишком тяжелая")
+        await send_photo(msg.from_user.id, 'dataset/witcher.png',
+                         "Хмм, зараза... Что-то не так, возможно фотка слишком тяжелая")
 
 
 @dp.message_handler(commands=['game'])
@@ -31,14 +33,24 @@ async def start_game(msg: types.Message):
     new_img_id = randint(1, 17)
     players_array[msg.from_user.id] = {'playing': True, 'prev_img': new_img_id}
     await msg.reply("Ну давайте немного поиграем:")
-    await bot.send_photo(msg.from_user.id, open(f'dataset/{new_img_id}.jpg', 'rb').read(),
-                         caption="Сколько лет Вы дадите этому человеку?")
+    await send_photo(msg.from_user.id, f'dataset/{new_img_id}.jpg', "Сколько лет Вы дадите этому человеку?")
 
 
 @dp.message_handler(commands=['stop'])
-async def stop(message: types.Message):
-    players_array[message.from_user.id]['playing'] = False
-    await message.reply("Игра окончена")
+async def stop(msg: types.Message):
+    if msg.from_user.id in players_array:
+        players_array[msg.from_user.id]['playing'] = False
+        await msg.reply("Игра окончена")
+
+
+@dp.message_handler(commands=['send_all'])
+async def send_all(msg: types.Message):
+    await msg.reply(r"Сам попросил ¯\_(ツ)_/¯")
+    for file in sorted(listdir('dataset')):
+        if file.endswith('jpg'):
+            await send_photo(msg.from_user.id, f'dataset/{file}')
+
+    print(len(all_files))
 
 
 @dp.message_handler(commands=['start'])
@@ -75,15 +87,27 @@ async def number_end(msg: types.Message):
             ans = "Совсем далеко! " + shablon
 
         await bot.send_message(msg.from_user.id, ans)
-        await bot.send_photo(msg.from_user.id,
-                             open(f"dataset/{prev_img_id}_process.jpg", 'rb').read(),
-                             caption="А вот, что об этом думает наша нейросеть")
-
-        await bot.send_photo(msg.from_user.id, open(f'dataset/{new_img_id}.jpg', 'rb').read(),
-                             caption="А сколько лет Вы дадите этому человеку?")
+        await send_photo(msg.from_user.id, f"dataset/{prev_img_id}_process.jpg",
+                         "А вот, что об этом думает наша нейросеть", 2)
+        await send_photo(msg.from_user.id, f'dataset/{new_img_id}.jpg', "А сколько лет Вы дадите этому человеку?", 4)
     else:
         await bot.send_message(msg.from_user.id, "Извините, я Вас не понимаю\nПросто напомню, что есть команда /help")
 
 
 def get_age_by_id(photo_id):
     return int(re.findall(r'\d{1,4}', age_dataset[photo_id - 1])[0])
+
+
+async def send_photo(to, filename, caption="", sleep_time=0):
+    await bot.send_chat_action(to, 'upload_photo')
+    await asyncio.sleep(sleep_time)
+
+    if filename in all_files:
+        file_tusent = all_files[filename]
+    else:
+        file_tusent = open(filename, 'rb').read()
+
+    sended_photo = await bot.send_photo(to, file_tusent, caption=caption)
+
+    if filename not in all_files:
+        all_files[filename] = sended_photo.photo[0].file_id
--- misc.py
+++ misc.py
@@ -2,7 +2,7 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-TOKEN = open('TOKEN').read()
+TOKEN = open('TOKEN').readlines()[0].strip()
 
 bot = Bot(token=TOKEN)
 memory_storage = MemoryStorage()
